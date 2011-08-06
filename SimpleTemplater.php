6822e029d0c2ff653cae0640d8867415e8e6adb5
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2011-08-06 09:47:28 -0500

    SimpleTemplater.php : getters/setters

--- SimpleTemplater.php
+++ SimpleTemplater.php
@@ -8,34 +8,50 @@
  */
 class SimpleTemplater {
 	
-	private $data;
-	private $template;
+	private $_data;
+	private $_template;
 	
 	public function __construct ( $template='', $data=array() ) {
-		$this->template = $template;
-		$this->data = $data;
+		$this->_template = $template;
+		$this->_data = $data;
 	}
 	
-	public function setTemplate( $newTemplate = '' ) {
-		$this->template = $newTemplate;
+	public function __get( $key ) {
+		switch ($key) {
+			//private read
+			case 'data':
+				return;
+			//public read
+			default:
+				return $this->{"_$key"};
+		}
 	}
 	
-	public function setData( $newData = array() ) {
-		$this->data = $newData;
+	public function __set( $key, $value ) {
+		switch ($key) {
+			//private write
+			case '':
+				break;
+			//public write
+			default:
+				$this->{"_$key"} = $value;
+		}
 	}
 	
 	public function render() {
 		
+		$rendering = $this->_template;
+		
 		// First do regular variable replacements.
 		$matches = array();
-		preg_match_all( '/{{([^:{}]+).*?}}/', $this->template, $matches );
+		preg_match_all( '/{{([^:{}]+).*?}}/', $rendering, $matches );
 		
 		$matched = array_slice($matches, 1);
 		$matched = $matched[0];
 		
-		$rendering = $this->template;
+		$rendering = $this->_template;
 		foreach ($matched as $i => $name) {
-			$value = $this->data[$name];
+			$value = $this->_data[$name];
 			$rendering = preg_replace( '/{{'.$name.'}}/', $value, $rendering );
 		}
 		
@@ -47,7 +63,7 @@ class SimpleTemplater {
 		$conditionals = $matched[1];
 		
 		foreach ($varNames as $i => $name) {
-			if ( isset($this->data[$name]) ) {
+			if ( isset($this->_data[$name]) ) {
 				$value = $conditionals[$i];
 			} else {
 				$value = '';
